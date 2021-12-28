1c1d98dc1622d19b852a6a0618c4df27d1184a81
Lluroa <luora@naver.com>
2021-12-28 13:24:00 +0900

    [add] AOP가 필요한 상황 [#18]

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -22,11 +22,20 @@ public class MemberService {
 
     //회원가입
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
 
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally { // 로직이 끝날 때
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
+
+
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
@@ -37,8 +46,16 @@ public class MemberService {
 
     //전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll(); // 반환타입 List<Member>
+        long start = System.currentTimeMillis();
+        try{
+            return memberRepository.findAll(); // 반환타입 List<Member>
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
+
 
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -39,10 +39,10 @@ class MemberServiceIntegrationTest {
     public void 중복_회원_예외() throws Exception {
         //Given
         Member member1 = new Member();
-        member1.setName("spring");
+        member1.setName("spring1");
 
         Member member2 = new Member();
-        member2.setName("spring");
+        member2.setName("spring1");
 
         //When
         memberService.join(member1);
